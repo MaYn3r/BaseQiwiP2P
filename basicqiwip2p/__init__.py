39d6dfece0d657524385c6b9d750e2d5224db0ff
MaYn3r <mayneryt@bk.ru>
2023-06-07 23:52:12 +0300

    'fixed'

--- basicqiwip2p/__init__.py
+++ basicqiwip2p/__init__.py
@@ -1,5 +1,5 @@
-from aiobasicqiwip2p import AioBasicQiwiP2P
-from basicqiwip2p import BasicQiwiP2P
+from basicqiwip2p.aiobasicqiwip2p import AioBasicQiwiP2P
+from basicqiwip2p.basicqiwip2p import BasicQiwiP2P
 
 
 __all__ = ['AioBasicQiwiP2P', 'BasicQiwiP2P']
